97a80ba66b56052b115c083cd141fe377bd95e8a
Ruhisoles <ruhisoles@gmail.com>
2024-07-29 15:57:30 +0200

    Update week 1 and week 2

---
+++ Demo-2/Demo-ShopMVC-1/MyShop/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
+
+app.Run();
