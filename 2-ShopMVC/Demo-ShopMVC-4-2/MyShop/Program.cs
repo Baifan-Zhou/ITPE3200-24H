84ea174ff1eec186d90a9427696e1362766e4de2
Ruhisoles <ruhisoles@gmail.com>
2024-07-29 19:51:15 +0200

    complete mvc module

---
+++ 2-ShopMVC/Demo-ShopMVC-4-2/MyShop/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+app.MapDefaultControllerRoute();
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
